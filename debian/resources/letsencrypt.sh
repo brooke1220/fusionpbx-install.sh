c7e424431a138439b0b62c2bdc4153f908d364a4
brooke <2885270791@qq.com>
2021-06-04 09:11:21 +0800

    1

--- debian/resources/letsencrypt.sh
+++ debian/resources/letsencrypt.sh
@@ -23,7 +23,7 @@ read -p 'Domain Name: ' domain_name
 read -p 'Email Address: ' email_address
 
 #get and install dehydrated
-cd /usr/src && git clone https://github.com/lukas2511/dehydrated.git
+cd /usr/src && git clone https://github.com.cnpmjs.org/dehydrated-io/dehydrated.git
 cd /usr/src/dehydrated
 cp dehydrated /usr/local/sbin
 mkdir -p /var/www/dehydrated
@@ -45,7 +45,7 @@ fi
 #manual dns hook
 if [ .$wildcard_domain = ."true" ]; then
     cd /usr/src
-    git clone https://github.com/gheja/dns-01-manual.git
+    git clone https://github.com.cnpmjs.org/gheja/dns-01-manual.git
     cd /usr/src/dns-01-manual/
     cp hook.sh /etc/dehydrated/hook.sh
     chmod 755 /etc/dehydrated/hook.sh
--- debian/resources/sngrep.sh
+++ debian/resources/sngrep.sh
@@ -12,7 +12,7 @@ cd "$(dirname "$0")"
 if [ ."$cpu_architecture" = ."arm" ]; then
 	#source install
 	apt-get install -y git autoconf automake gcc make libncurses5-dev libpcap-dev libssl-dev libpcre3-dev
-	cd /usr/src && git clone https://github.com/irontec/sngrep
+	cd /usr/src && git clone https://github.com.cnpmjs.org/irontec/sngrep.git
 	cd /usr/src/sngrep && ./bootstrap.sh
 	cd /usr/src/sngrep && ./configure
 	cd /usr/src/sngrep && make install
